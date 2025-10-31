4b950af8e364c46e4f2ed06d51501ba2f8107537
Hashir <70011762+HashMaster02@users.noreply.github.com>
2025-10-30 21:53:11 -0500

    Can now close the mobile nav menu by clicking outside

--- src/scripts/mobile-nav.js
+++ src/scripts/mobile-nav.js
@@ -1,16 +1,29 @@
 window.onload = function () {
+  let show = false;
+  document.addEventListener("click", function (e) {
+    e.stopPropagation();
+    const specificDiv = document.querySelector(".nav-links-mobile");
+
+    if (show && !specificDiv.contains(event.target)) {
+      closeNav();
+    }
+  });
+
   document.getElementById("hamburger-icon").addEventListener("click", openNav);
   document
     .getElementById("close-hamburger-menu")
     .addEventListener("click", closeNav);
 
-  function openNav() {
+  function openNav(e) {
+    e.stopPropagation();
     document.querySelector(".nav-links-mobile").style.padding = "2rem 1rem";
     document.querySelector(".nav-links-mobile").style.width = "200px";
+    show = true;
   }
 
   function closeNav() {
     document.querySelector(".nav-links-mobile").style.padding = "0";
     document.querySelector(".nav-links-mobile").style.width = "0";
+    show = true;
   }
 };
